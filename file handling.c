89107438281b168fbbdc6a34e606fd2cee132677
4K7-Coder <78072738+4K7-Coder@users.noreply.github.com>
2021-02-03 21:49:17 +0600

    Add files via upload

---
+++ file handling.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+int main()
+{
+	FILE *file1;
+	FILE *file2;
+	int bytes,i=0;
+	file1=fopen("file1.txt","r");
+	file2=fopen("file2.txt","w");
+	
+	if(file1==NULL)
+	{
+		printf("File doesn't exist");
+		return 0;
+	}
+	printf("file1 opened");
+	fseek(file1, 0, SEEK_END);
+	bytes=ftell(file1);
+	while(i<bytes)
+	{
+		i++;
+		fseek(file1, -i, SEEK_END);
+		fprintf(file2, "%c", fgetc(file1));
+	}
+	printf("\nOperation successful");
+	fclose(file1);
+	fclose(file2);
+	return 0;
+}
