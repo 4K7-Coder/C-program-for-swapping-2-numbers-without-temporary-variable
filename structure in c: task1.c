2fbba1a449c735d8d570671a4fb7266893d70eb1
4K7-Coder <78072738+4K7-Coder@users.noreply.github.com>
2021-02-03 14:49:17 +0600

    Add files via upload

---
+++ structure in c: task1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+struct employee
+{
+  char name[50];
+  int age;
+  int ph_no;
+  float salary;
+};
+int main()
+{
+  struct employee e[20];
+  int i;
+ 
+ for(i=0;i<20;i++) 
+  {
+    printf("Enter information for employee %d\n",i+1);
+    printf("Enter name : ");
+    scanf("%s",&e[i].name);
+    printf("Enter Age : ");
+    scanf("%d",&e[i].age);
+    printf("Enter PhoneNumber : ");
+    scanf("%d",&e[i].ph_no);
+    printf("Enter Salary : ");
+    scanf("%d",&e[i].salary);
+  }
+  printf("\nName \tAge \tPhoneNumber \tSalary\n");
+ for(i=0;i<20;i++) 
+  {
+    printf("%s \t%d \t%d \t%d\n",e[i].name, e[i].age, e[i].ph_no, e[i].salary);
+  }
+}
